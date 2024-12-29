6e2bc4d3e523f2afe0a1a023a56349784dd2f557
voin12k <vladleurda02@gmail.com>
2024-12-29 18:02:24 +0100

    cors done

--- src/app.js
+++ src/app.js
@@ -4,12 +4,24 @@ const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const messageRoutes = require('./routes/messageRoutes');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/chats', chatRoutes);
 app.use('/api/messages', messageRoutes);
 
+
+const corsOptions = {
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, 
+};
+
+app.use(cors(corsOptions));
+
 module.exports = app;
+
