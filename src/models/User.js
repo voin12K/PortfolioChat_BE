a74ac222c3e68ec875a5e5193993d597ffda2f46
voin12k <vladleurda02@gmail.com>
2025-01-04 20:03:16 +0100

    user update 2

--- src/models/User.js
+++ src/models/User.js
@@ -10,20 +10,14 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
 }, { timestamps: true });
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 userSchema.methods.toJSON = function () {
     const userObject = this.toObject();
     delete userObject.password;
     return userObject;
-};
-
-userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
 };
 
 const User = mongoose.model('User', userSchema);
